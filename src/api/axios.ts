cf12a34fe4b3233f4cc9039eac2a762263bc6ef2
Nikhil Dhanda <nikhildhanda84@gmail.com>
2025-07-06 16:48:30 +0530

    changed backend deployed link

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://roadmap-backend-1.onrender.com' ,
+  baseURL: 'https://roadmap-backend-n5b0.onrender.com' ,
   withCredentials: true,
 });
 
